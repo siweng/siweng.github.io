d37f15ce831a0e9c067ce53e489e68e0a8967e35
Siweng <siweng@icloud.com>
2014-06-20 18:24:52 +0800

    auto commit

---
+++ config.rb
@@ -0,0 +1,11 @@
+require "susy"
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "_scss"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+output_style = :compressed
+
+color_output = true
